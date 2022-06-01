ce3aaae545665e5f081ba001b1c096846156a092
bazla <dobr1nya@mail.ru>
2022-06-01 22:07:21 +0300

    feat: implement delete-func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -24,5 +24,3 @@ export const copy = async () => {
     }
   });
 };
-
-copy();
--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,13 @@
+import fs from 'fs';
+
 export const remove = async () => {
-    // Write your code here 
+  if (fs.existsSync('./files/fileToRemove.txt')) {
+    fs.unlink('./files/fileToRemove.txt', (err) => {
+      if (err) {
+        throw new Error(err.message, 'err');
+      }
+    });
+  } else {
+    throw new Error('File "fileToRemove.txt" is missing ');
+  }
 };
