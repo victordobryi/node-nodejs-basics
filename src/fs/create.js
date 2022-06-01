2f96a104a807962bdc43547ecc16652c065707a8
bazla <dobr1nya@mail.ru>
2022-06-01 14:54:10 +0300

    Revert "feat: implement create-function"
    This reverts commit 61af6daef19fb5eb6d838e4018ee294864d0c442.

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,13 +1,3 @@
-import fs from 'fs';
-
 export const create = async () => {
-  if (fs.existsSync('./files/fresh.txt')) {
-    throw new Error('File already exists');
-  } else {
-    fs.writeFile('./files/fresh.txt', 'I am fresh and young', (err) => {
-      if (err) {
-        throw new Error(err.message);
-      }
-    });
-  }
+    // Write your code here 
 };
