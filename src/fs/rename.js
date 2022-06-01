9c0bdee48d79b5583ffd8d3c03cb63a65aef3548
bazla <dobr1nya@mail.ru>
2022-06-01 21:43:29 +0300

    feat: implement rename-function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,17 @@
+import fs from 'fs';
+
 export const rename = async () => {
-    // Write your code here 
+  if (fs.existsSync('./files/wrongFilename.txt')) {
+    if (fs.existsSync('./files/properFilename.md')) {
+      throw new Error('File "properFilename.md" already exists');
+    } else {
+      fs.rename('./files/wrongFilename.txt', './files/properFilename.md', (err) => {
+        if (err) {
+          throw new Error(err.message, 'err');
+        }
+      });
+    }
+  } else {
+    throw new Error('File "wrongFilename.txt" is missing ');
+  }
 };
