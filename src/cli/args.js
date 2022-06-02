0bf525daff92acf1917de61b2196d9da1f1bf581
bazla <dobr1nya@mail.ru>
2022-06-02 11:25:17 +0300

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,9 @@
 export const parseArgs = () => {
-    // Write your code here 
+  process.argv.map((item, i, arr) => {
+    item.match(/--[a-zA-Z]/) && arr[i + 1] && !arr[i + 1].match(/--[a-zA-Z]/)
+      ? console.log(`${item.replace('--', '')} is ${arr[i + 1]}`)
+      : null;
+  });
 };
+
+parseArgs();
