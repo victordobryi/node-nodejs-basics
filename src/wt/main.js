28a245236ff44b3d70decb5082a1be10ddf7a314
bazla <dobr1nya@mail.ru>
2022-06-04 13:15:56 +0300

    feat: implement sendResult, performCalculations functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,24 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 export const performCalculations = async () => {
-    // Write your code here
+  const corsCount = os.cpus().length;
+  const resultsArr = [];
+
+  for (let i = 0; i < corsCount; i++) {
+    const currentNum = 10 + i;
+    const worker = new Worker('./worker.js', {
+      workerData: currentNum,
+    });
+    resultsArr.push(
+      new Promise((res, rej) => {
+        worker.on('message', (result) => {
+          res(`${currentNum}th Fibonacci No: ${result}`);
+        });
+      })
+    );
+  }
+  await Promise.all(resultsArr).then((res) => console.log(res));
 };
+
+performCalculations();
