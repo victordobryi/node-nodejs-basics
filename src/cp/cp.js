d7ba396ac21391780b33fbe2087bbaac3b3ff8fa
bazla <dobr1nya@mail.ru>
2024-01-28 17:03:04 +0300

    feat: add spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,21 +1,10 @@
+import { fork } from 'child_process';
+import { getAbsPath } from '../utils/getAbsPath.js';
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const cpFilePath = getAbsPath(import.meta.url, `/files/script.js`);
+
+  const child = fork(cpFilePath, args);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
-
-// CODE FROM NODEJS2022Q2
-
-// import { fork } from 'child_process';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// export const spawnChildProcess = async (args) => {
-//   const child = fork(__dirname + '/files/script.js', args);
-// };
-
-// spawnChildProcess(['1', '2', '3']);
+spawnChildProcess(['1', '2', '3']);
