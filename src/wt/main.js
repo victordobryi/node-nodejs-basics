9ec22940ee9e9d886d5e2a8c5041a5cfb097e948
bazla <dobr1nya@mail.ru>
2024-01-28 17:00:21 +0300

    feat: add performCalculations function

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,37 +1,31 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import { getAbsPath } from '../utils/getAbsPath.js';
+
 const performCalculations = async () => {
-  // Write your code here
+  const workerFilePath = getAbsPath(import.meta.url, `/worker.js`);
+
+  try {
+    const corsCount = os.cpus().length;
+    const resultsArr = [];
+
+    for (let i = 0; i < corsCount; i++) {
+      const currentNum = 10 + i;
+      const worker = new Worker(workerFilePath, {
+        workerData: currentNum,
+      });
+      resultsArr.push(
+        new Promise((res, rej) => {
+          worker.on('message', (result) => {
+            res(`${currentNum}th Fibonacci No: ${result}`);
+          });
+        })
+      );
+    }
+    await Promise.all(resultsArr).then((res) => console.log(res));
+  } catch (err) {
+    console.log(`Operation failed : ${err}`);
+  }
 };
 
 await performCalculations();
-
-// CODE FROM NODEJS2022Q2
-
-// import { Worker } from 'worker_threads';
-// import os from 'os';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// export const performCalculations = async () => {
-//   const corsCount = os.cpus().length;
-//   const resultsArr = [];
-
-//   for (let i = 0; i < corsCount; i++) {
-//     const currentNum = 10 + i;
-//     const worker = new Worker(__dirname + '/worker.js', {
-//       workerData: currentNum,
-//     });
-//     resultsArr.push(
-//       new Promise((res, rej) => {
-//         worker.on('message', (result) => {
-//           res(`${currentNum}th Fibonacci No: ${result}`);
-//         });
-//       })
-//     );
-//   }
-//   await Promise.all(resultsArr).then((res) => console.log(res));
-// };
-
-// performCalculations();
