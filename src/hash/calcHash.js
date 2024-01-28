54498accd5554c0ba060f2b6618517d837036ed4
bazla <dobr1nya@mail.ru>
2024-01-28 12:12:38 +0300

    fix: refactor calcHash function. add readStream

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,4 +1,4 @@
-import fs from 'fs/promises';
+import fs from 'fs';
 import crypto from 'crypto';
 import { getAbsPath } from '../utils/getAbsPath.js';
 import { CALC_HASH_FILE_NAME } from '../constants/fs.js';
@@ -6,12 +6,30 @@ import { CALC_HASH_FILE_NAME } from '../constants/fs.js';
 const calculateHash = async () => {
   try {
     const cachHashFilePath = getAbsPath(import.meta.url, `/files/${CALC_HASH_FILE_NAME}`);
-    const fileData = await fs.readFile(cachHashFilePath, 'utf-8');
-    const hash = crypto.createHash('sha256').update(fileData).digest('hex');
-    console.log(hash);
+    const hash = crypto.createHash('sha256');
+    const fileStream = fs.createReadStream(cachHashFilePath);
+
+    fileStream.on('data', (chunk) => {
+      hash.update(chunk);
+    });
+
+    fileStream.on('end', () => {
+      const hashValue = hash.digest('hex');
+      console.log(`Hash value : ${hashValue}`);
+
+      const endTime = Date.now();
+      console.log(`File calculate operation completed in ${(endTime - startTime) / 1000} seconds.`);
+    });
+
+    fileStream.on('error', (error) => {
+      reject(error);
+    });
   } catch (err) {
     console.log(`Operation failed : ${err}`);
   }
 };
+
+console.log('Start calculating hash value...');
+const startTime = Date.now();
 
 await calculateHash();
